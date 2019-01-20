e17cf684320a49695dffac4a494c4cbf02ee9eaa
myszon3334 <33763181+mySzon3334@users.noreply.github.com>
2019-01-20 18:22:28 +0100

    added some things

---
+++ settings.py
@@ -0,0 +1,23 @@
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+
+class Theme:
+    def __init__(self, background_color, text_color, player1_color, player2_color, board_color):
+        self.background_color = background_color
+        self.text_color = text_color
+        self.player1_color = player1_color
+        self.player2_color = player2_color
+        self.board_color = board_color
+
+
+classic_theme = Theme(white, black, red, blue, black)
+
+
+def settings_menu():
+    exit = False
+    #while not exit:
+        #draw_settings_menu
